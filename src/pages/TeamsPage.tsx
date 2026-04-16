f0fc3483f3b032004e54be3e0ad6e720bb918602
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-16 11:15:00 +0200

    TeamsPage layout

--- src/pages/TeamsPage.tsx
+++ src/pages/TeamsPage.tsx
@@ -1,9 +1,36 @@
 import classes from "@/pages/TeamsPage.module.css";
+import { X } from "lucide-react";
+import { useId } from "react";
 
 export default function TeamsPage() {
+  const inputFilterId = useId();
   return(
-    <div>
-      TeamsPage
+    <div className={classes.teamsMainContainer}>
+      <div className={classes.filterRender}>
+        <form 
+          onSubmit={(e) => e.preventDefault()}
+          className={classes.form}
+        >
+          <input 
+            type="text" 
+            id={inputFilterId}
+            className={classes.teamsFilterInput} 
+          />
+          <button
+            className={classes.formButton}
+            onClick={() => console.log("btn")}
+            disabled={true}
+          >
+            <X />
+          </button>
+        </form>
+        <div>
+          
+        </div>
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
