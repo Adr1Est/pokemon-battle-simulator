ca862f6d96220fc30024b44fdb58d1e8254e12c0
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-16 15:51:20 +0200

    useTeamBuilder to save uncompleted team

--- src/store/useTeamBuilder.ts
+++ src/store/useTeamBuilder.ts
@@ -1,9 +1,31 @@
-import type { PokemonTeam } from "@/types/pokemon.types";
+import type { Pokemon } from "@/types/pokemon.types";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 interface TeamBuilderStore {
-  teamLayout: PokemonTeam;
+  teamLayout: Pokemon[];
   
-  addPokemonToTeam: ()
+  addPokemonToTeam: (pokemon: Pokemon) => void;
+  removePokemonFromTeam: (pokemonId: number) => void;
+  resetTeam: () => void;
 }
+
+export const useTeamBuilder = create<TeamBuilderStore>()(
+  persist(
+    (set) => ({
+      teamLayout: [],
+
+      addPokemonToTeam: (pokemon) => set((state) => ({
+        teamLayout: state.teamLayout.some((p) => p.id === pokemon.id)
+          ? state.teamLayout
+          : [...state.teamLayout, pokemon]
+      })),
+      removePokemonFromTeam: (pokemonId) => set((state) => ({
+        teamLayout: state.teamLayout.filter((p) => p.id !== pokemonId),
+      })),
+
+      resetTeam: () => set(() => ({ teamLayout: [] }))
+    }),
+    { name: "wip-team" }
+  )
+);
