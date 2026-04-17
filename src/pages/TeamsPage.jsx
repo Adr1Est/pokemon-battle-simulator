3b32c150d74dd56fc498c9b0183d3a9b0f48e914
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2026-04-17 11:41:08 +0200

    teams route

---
+++ src/pages/TeamsPage.jsx
@@ -0,0 +1,9 @@
+import classes from "@/pages/TeamsPage.module.css"
+
+export default function TeamsPage() {
+  return (
+    <div>
+      <p>teamsPage</p>
+    </div>
+  )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import App from "@/App";
 import CreateTeamPage from "@/pages/CreateTeamPage";
 import NotFoundPage from "@/pages/NotFoundPage";
+import TeamsPage from "@/pages/TeamsPage";
 
 const router = createBrowserRouter([
   {
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
     Component: App,
     children: [
       { index: true, Component: CreateTeamPage },
+      { path: "teams", Component: TeamsPage },
       { path: "*", Component: NotFoundPage },
     ],
   },
